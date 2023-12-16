ccde9eea4685c09ef782fa88c2244a650076af06
JeffYan <jeffyan167@gmail.com>
2023-12-16 11:44:54 +0800

    上次提交的代码会出现卡住的情况，这次修改代码后成功完成测试

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -5,7 +5,10 @@ int ll_has_cycle(node *head) {
     /* your code here */
     node *p_st_slow = head;
     node *p_st_fast = head;
-    while(NULL != p_st_fast && NULL != p_st_fast->next) {
+    while(NULL != p_st_fast) {
+        if (!p_st_fast->next) {
+            return 0;
+        }
         p_st_slow = p_st_slow->next;
         p_st_fast = p_st_fast->next->next;
         if (p_st_slow == p_st_fast) {
