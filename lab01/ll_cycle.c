ada47f67d7ac0a9b38f3f06cd02fb08fd0275f62
JeffYan <jeffyan167@gmail.com>
2023-12-16 10:55:10 +0800

    lab01的Exercise 5完成，本次练习写了一个判断单链表有无环的程序，判断成功

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *p_st_slow = head;
+    node *p_st_fast = head;
+    while(NULL != p_st_fast && NULL != p_st_fast->next) {
+        p_st_slow = p_st_slow->next;
+        p_st_fast = p_st_fast->next->next;
+        if (p_st_slow == p_st_fast) {
+            return 1;
+        }
+    }
     return 0;
 }
